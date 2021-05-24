0cc1e3e043d04a372098f416db7eedbcc92992c0
tonidy <toni_d_y@live.com>
2021-05-24 22:53:18 +0700

    Try to create patch

--- Formula/Mkpasswd.rb
+++ Formula/Mkpasswd.rb
@@ -1,11 +1,13 @@
 class Mkpasswd < Formula
   desc "mkpasswd tool to make password tool"
   homepage "https://packages.debian.org/sid/whois"
-  url "https://github.com/tonidy/whois/archive/3be4b59af6d601c3b8f2d2634459fc31b1bf7155.zip"
+  url "https://github.com/rfc1036/whois/archive/refs/tags/v5.5.9.zip"
   sha256 "b946386e2406991a855b935ad4efca87767bb65df80d5616d08c88a3ec716aaa"
+  version "5.5.9"
   license "GPL-2.0-or-later"
   head "https://github.com/tonidy/whois.git", :branch => "master" 
 
+  bottle :unneeded
   keg_only :provided_by_macos
 
   depends_on "pkg-config" => :build
@@ -14,8 +16,8 @@ class Mkpasswd < Formula
   def install
     on_macos do
       ENV.append "LDFLAGS", "-L/usr/lib -liconv"
-      ENV.append "LDFLAGS", "-L/usr/local/opt/openssl@1.1/lib"
-      ENV.append "CPPFLAGS", "-I/usr/local/opt/openssl@1.1/include"
+      # ENV.append "LDFLAGS", "-L/usr/local/opt/openssl@1.1/lib"
+      # ENV.append "CPPFLAGS", "-I/usr/local/opt/openssl@1.1/include"
     end
 
     have_iconv = "HAVE_ICONV=1"
@@ -25,8 +27,8 @@ class Mkpasswd < Formula
     end
 
     system "make", "mkpasswd", have_iconv
-    bin.install "mkpasswd"
-    man1.install "mkpasswd.1"
+    bin.install "bin/mkpasswd"
+    man1.install "man/mkpasswd.1"
   end
 
   test do
