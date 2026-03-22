4bdf55f5ebfa430c8f4f8c63f0ccef6202d77864
Toni D <tonidy@outlook.com>
2026-03-22 07:51:06 +0700

    mkpasswd: fix defines order, bump to 5.6.6_9

--- Formula/mkpasswd.rb
+++ Formula/mkpasswd.rb
@@ -3,7 +3,7 @@ class Mkpasswd < Formula
   homepage "https://packages.debian.org/sid/whois"
   url "https://github.com/rfc1036/whois/archive/refs/tags/v5.6.6.zip"
   sha256 "3418aa374858199e14a229b89547bc1aca6412ebf18bc3be11cb9d7268bf562a"
-  version "5.6.6_8"
+  version "5.6.6_9"
   license "GPL-2.0-or-later"
   head "https://github.com/rfc1036/whois.git", :branch => "master" 
 
@@ -11,33 +11,20 @@ class Mkpasswd < Formula
   depends_on "openssl"
 
   def install
-    # Read source files
+    # Fix mkpasswd.c - reorder defines and includes
     mkpasswd_content = File.read("mkpasswd.c")
+    mkpasswd_content = mkpasswd_content.sub(
+      "#define _XOPEN_SOURCE 500\n#define _BSD_SOURCE 1\n#define _DEFAULT_SOURCE 1\n#define __EXTENSIONS__ 1",
+      "#ifndef _XOPEN_SOURCE\n#define _XOPEN_SOURCE 500\n#endif\n#ifndef _BSD_SOURCE\n#define _BSD_SOURCE 1\n#endif\n#ifndef _DEFAULT_SOURCE\n#define _DEFAULT_SOURCE 1\n#endif"
+    )
+    File.write("mkpasswd.c", mkpasswd_content)
+
+    # Fix utils.c - add proper defines
     utils_content = File.read("utils.c")
-
-    puts "mkpasswd.c first 5 lines: #{mkpasswd_content.lines.first(5).join}"
-    puts "utils.c first 5 lines: #{utils_content.lines.first(5).join}"
-
-    # Fix mkpasswd.c - add includes at the very beginning
-    unless mkpasswd_content.include?('#include <stdio.h>')
-      mkpasswd_content = '#include <stdio.h>
-#include <string.h>
-' + mkpasswd_content
-      File.write("mkpasswd.c", mkpasswd_content)
-      puts "Wrote mkpasswd.c with includes"
-    else
-      puts "mkpasswd.c already has includes, skipping"
+    unless utils_content.include?('#define _XOPEN_SOURCE')
+      utils_content = "#define _XOPEN_SOURCE 500\n" + utils_content
     end
-
-    # Fix utils.c - add includes at the very beginning
-    unless utils_content.include?('#include <string.h>')
-      utils_content = '#include <string.h>
-' + utils_content
-      File.write("utils.c", utils_content)
-      puts "Wrote utils.c with includes"
-    else
-      puts "utils.c already has includes, skipping"
-    end
+    File.write("utils.c", utils_content)
 
     # Add pkg-config support for libcrypto in Makefile
     inreplace "Makefile", /^else$/, "else ifeq ($(shell $(PKG_CONFIG) --exists 'libcrypto' || echo NO),)\n  mkpasswd_LDADD += $(shell $(PKG_CONFIG) --libs libcrypto)\nelse"
