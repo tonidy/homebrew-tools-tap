8c53f7830357f62b993825ca704602a924bbff9b
Toni D <tonidy@outlook.com>
2026-03-22 07:49:47 +0700

    mkpasswd: debug includes, bump to 5.6.6_8

--- Formula/mkpasswd.rb
+++ Formula/mkpasswd.rb
@@ -3,7 +3,7 @@ class Mkpasswd < Formula
   homepage "https://packages.debian.org/sid/whois"
   url "https://github.com/rfc1036/whois/archive/refs/tags/v5.6.6.zip"
   sha256 "3418aa374858199e14a229b89547bc1aca6412ebf18bc3be11cb9d7268bf562a"
-  version "5.6.6_7"
+  version "5.6.6_8"
   license "GPL-2.0-or-later"
   head "https://github.com/rfc1036/whois.git", :branch => "master" 
 
@@ -15,12 +15,18 @@ class Mkpasswd < Formula
     mkpasswd_content = File.read("mkpasswd.c")
     utils_content = File.read("utils.c")
 
+    puts "mkpasswd.c first 5 lines: #{mkpasswd_content.lines.first(5).join}"
+    puts "utils.c first 5 lines: #{utils_content.lines.first(5).join}"
+
     # Fix mkpasswd.c - add includes at the very beginning
     unless mkpasswd_content.include?('#include <stdio.h>')
       mkpasswd_content = '#include <stdio.h>
 #include <string.h>
 ' + mkpasswd_content
       File.write("mkpasswd.c", mkpasswd_content)
+      puts "Wrote mkpasswd.c with includes"
+    else
+      puts "mkpasswd.c already has includes, skipping"
     end
 
     # Fix utils.c - add includes at the very beginning
@@ -28,6 +34,9 @@ class Mkpasswd < Formula
       utils_content = '#include <string.h>
 ' + utils_content
       File.write("utils.c", utils_content)
+      puts "Wrote utils.c with includes"
+    else
+      puts "utils.c already has includes, skipping"
     end
 
     # Add pkg-config support for libcrypto in Makefile
