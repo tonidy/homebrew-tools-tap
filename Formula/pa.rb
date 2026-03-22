bd0262ca19c3a70e73b276b59f2e160bfdae04e9
Toni D <tonidy@outlook.com>
2026-03-22 11:30:39 +0700

    fix(formula(pa)): set license to AGPL-3.0
    standardize license field to AGPL-3.0 to align with upstream

--- Formula/pa.rb
+++ Formula/pa.rb
@@ -3,7 +3,7 @@ class Pa < Formula
   homepage "https://github.com/tonidy/pa-cli"
   url "https://github.com/tonidy/pa-cli/archive/refs/tags/v1.2.1.tar.gz"
   sha256 "0de7f86431134a7e357f3d03580dd1a8ea9125fd2636c045c11a690c879cd3e3"
-  license "AGPL-3.0-or-later"
+  license "AGPL-3.0"
   revision 1
 
   depends_on "age"
