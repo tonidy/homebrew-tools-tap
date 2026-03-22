b078d81f14dcaff1bd171ac8c6c3443f8ca8dc01
Toni D <tonidy@outlook.com>
2026-03-22 10:28:06 +0700

    build(formula): patch mkpasswd build for macOS headers and libcrypto
    update version to 5.6.6 and set revision to 1
    patch mkpasswd.c to prepend headers and adjust utils.c
    patch Makefile to respect libcrypto via pkg-config
    on macOS, link system iconv in LDFLAGS
    adjust install to compile and install binary and manpage

--- Formula/mkpasswd.rb
+++ Formula/mkpasswd.rb
@@ -3,24 +3,36 @@ class Mkpasswd < Formula
   homepage "https://github.com/rfc1036/whois"
   url "https://github.com/rfc1036/whois/archive/refs/tags/v5.6.6.tar.gz"
   sha256 "43d3b3cc64c75e8bd10aee6feff3906e9488ed335076d206e70f3b25bf644969"
-  version "5.6.6_12"
+  version "5.6.6"
+  revision 1
   license "GPL-2.0-or-later"
 
   depends_on "openssl@3"
 
   def install
-    openssl = Formula["openssl@3"]
+  # Prepend correct headers to mkpasswd.c
+  inreplace "mkpasswd.c", /^(.*)/, "#include <stdio.h>\n#include <string.h>\n\\1"
 
-    ENV.append "CPPFLAGS", "-I#{openssl.opt_include}"
-    ENV.append "LDFLAGS", "-L#{openssl.opt_lib}"
+  # Prepend string.h to utils.c
+  inreplace "utils.c", /^(.*)/, "#include <string.h>\n\\1"
 
-    have_iconv = OS.mac? ? "HAVE_ICONV=1" : "HAVE_ICONV=0"
+  # Patch Makefile for pkg-config libcrypto
+  inreplace "Makefile", /^else$/, <<~EOS
+    else ifeq ($(shell $(PKG_CONFIG) --exists 'libcrypto' || echo NO),)
+      mkpasswd_LDADD += $(shell $(PKG_CONFIG) --libs libcrypto)
+    else
+  EOS
 
-    system "make", "mkpasswd", have_iconv
-
-    bin.install "mkpasswd"
-    man1.install "mkpasswd.1"
+  on_macos do
+    ENV.append "LDFLAGS", "-L/usr/lib -liconv"
   end
+
+  have_iconv = OS.mac? ? "HAVE_ICONV=1" : "HAVE_ICONV=0"
+
+  system "make", "mkpasswd", have_iconv
+  bin.install "mkpasswd"
+  man1.install "mkpasswd.1"
+end
 
   test do
     output = shell_output("#{bin}/mkpasswd password")
