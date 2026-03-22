08f0f392ffac54fb9f05540535aba753ab77b696
Toni D <tonidy@outlook.com>
2026-03-22 07:33:03 +0700

    mkpasswd: fix inreplace syntax for newer Homebrew, bump to 5.6.6_2

--- Formula/mkpasswd.rb
+++ Formula/mkpasswd.rb
@@ -3,7 +3,7 @@ class Mkpasswd < Formula
   homepage "https://packages.debian.org/sid/whois"
   url "https://github.com/rfc1036/whois/archive/refs/tags/v5.6.6.zip"
   sha256 "3418aa374858199e14a229b89547bc1aca6412ebf18bc3be11cb9d7268bf562a"
-  version "5.6.6_1"
+  version "5.6.6_2"
   license "GPL-2.0-or-later"
   head "https://github.com/rfc1036/whois.git", :branch => "master" 
 
@@ -12,9 +12,7 @@ class Mkpasswd < Formula
 
   def install
     # Add pkg-config support for libcrypto in Makefile
-    inreplace "Makefile" do |s|
-      s.gsub(/^else$/, "else ifeq ($(shell $(PKG_CONFIG) --exists 'libcrypto' || echo NO),)\n  mkpasswd_LDADD += $(shell $(PKG_CONFIG) --libs libcrypto)\nelse")
-    end
+    inreplace "Makefile", /^else$/, "else ifeq ($(shell $(PKG_CONFIG) --exists 'libcrypto' || echo NO),)\n  mkpasswd_LDADD += $(shell $(PKG_CONFIG) --libs libcrypto)\nelse"
 
     on_macos do
       ENV.append "LDFLAGS", "-L/usr/lib -liconv"
