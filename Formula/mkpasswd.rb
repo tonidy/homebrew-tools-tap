8d93084dd9a8f0d9fd29b3b4d9e621c0ddefdf5d
Toni D <tonidy@outlook.com>
2026-03-22 10:23:14 +0700

    build(formula): update mkpasswd to 5.6.6_12 and switch to openssl@3
    - update to upstream tarball v5.6.6
    - bump version to 5.6.6_12
    - switch dependency to openssl@3
    - configure build via CPPFLAGS/LDFLAGS
    - remove in-tree include edits and Makefile patching
    - rely on system headers via CPPFLAGS/LDFLAGS
    - drop caveats and simplify HAVE_ICONV handling
    - adjust test to verify password generation output

--- Formula/mkpasswd.rb
+++ Formula/mkpasswd.rb
@@ -1,53 +1,29 @@
 class Mkpasswd < Formula
-  desc "mkpasswd: tool to make a password"
-  homepage "https://packages.debian.org/sid/whois"
-  url "https://github.com/rfc1036/whois/archive/refs/tags/v5.6.6.zip"
-  sha256 "3418aa374858199e14a229b89547bc1aca6412ebf18bc3be11cb9d7268bf562a"
-  version "5.6.6_11"
+  desc "Generate hashed passwords (mkpasswd from whois package)"
+  homepage "https://github.com/rfc1036/whois"
+  url "https://github.com/rfc1036/whois/archive/refs/tags/v5.6.6.tar.gz"
+  sha256 "43d3b3cc64c75e8bd10aee6feff3906e9488ed335076d206e70f3b25bf644969"
+  version "5.6.6_12"
   license "GPL-2.0-or-later"
-  head "https://github.com/rfc1036/whois.git", :branch => "master" 
 
-  depends_on "pkg-config" => :build
-  depends_on "openssl"
+  depends_on "openssl@3"
 
   def install
-    # Fix mkpasswd.c - add includes at absolute start
-    mkpasswd_content = File.read("mkpasswd.c")
-    mkpasswd_content = "#include <stdio.h>\n#include <string.h>\n" + mkpasswd_content
-    File.write("mkpasswd.c", mkpasswd_content)
+    openssl = Formula["openssl@3"]
 
-    # Fix utils.c - add includes at absolute start
-    utils_content = File.read("utils.c")
-    utils_content = "#include <string.h>\n" + utils_content
-    File.write("utils.c", utils_content)
+    ENV.append "CPPFLAGS", "-I#{openssl.opt_include}"
+    ENV.append "LDFLAGS", "-L#{openssl.opt_lib}"
 
-    # Add pkg-config support for libcrypto in Makefile
-    inreplace "Makefile", /^else$/, "else ifeq ($(shell $(PKG_CONFIG) --exists 'libcrypto' || echo NO),)\n  mkpasswd_LDADD += $(shell $(PKG_CONFIG) --libs libcrypto)\nelse"
-
-    on_macos do
-      ENV.append "LDFLAGS", "-L/usr/lib -liconv"
-    end
-
-    have_iconv = "HAVE_ICONV=1"
-
-    on_linux do
-      have_iconv = "HAVE_ICONV=0"
-    end
+    have_iconv = OS.mac? ? "HAVE_ICONV=1" : "HAVE_ICONV=0"
 
     system "make", "mkpasswd", have_iconv
+
     bin.install "mkpasswd"
     man1.install "mkpasswd.1"
   end
 
-  def caveats
-    return if OS.mac?
-    <<~EOS
-      Debian mkpasswd has been installed as `mkpasswd` and may shadow the
-      system binary of the same name.
-    EOS
-  end
-
   test do
-    system "#{bin}/mkpasswd", "test"
+    output = shell_output("#{bin}/mkpasswd password")
+    assert_match(/\$/, output)
   end
 end
