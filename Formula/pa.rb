ad9461f8d870b8c556f6a9f74fb61eebab3c36d5
Toni D <tonidy@outlook.com>
2026-03-22 11:35:12 +0700

    fix(pa): replace version placeholders during install
    - Patch PA_VERSION, PA_RELEASE_DATE, and PA_COMMIT placeholders
    - Show actual version instead of 'development'
    - Bump revision to 2

--- Formula/pa.rb
+++ Formula/pa.rb
@@ -4,16 +4,23 @@ class Pa < Formula
   url "https://github.com/tonidy/pa-cli/archive/refs/tags/v1.2.1.tar.gz"
   sha256 "0de7f86431134a7e357f3d03580dd1a8ea9125fd2636c045c11a690c879cd3e3"
   license "AGPL-3.0"
-  revision 1
+  revision 2
 
   depends_on "age"
   depends_on "fzf"
 
   def install
+    # Replace version placeholders (similar to Makefile install target)
+    inreplace "pa" do |s|
+      s.gsub!('PA_VERSION="__VERSION__"', "PA_VERSION=\"v1.2.1\"")
+      s.gsub!('PA_RELEASE_DATE="__RELEASE_DATE__"', "PA_RELEASE_DATE=\"Oct 24 2025\"")
+      s.gsub!('PA_COMMIT="__COMMIT__"', "PA_COMMIT=\"c6d63ee\"")
+    end
+
     bin.install "pa"
   end
 
   test do
-    assert_match(/^pa$/, `#{bin}/pa 2>&1 || true`)
+    assert_match(/pa version: v1.2.1/, `#{bin}/pa --version 2>&1`)
   end
 end
