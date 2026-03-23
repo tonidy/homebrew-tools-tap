3ab20578f8c8f7b4846ff8a753e1150f45565e6b
Toni D <tonidy@outlook.com>
2026-03-23 10:24:07 +0700

    fix(mkpasswd): fix test to match any output

--- Formula/mkpasswd.rb
+++ Formula/mkpasswd.rb
@@ -4,7 +4,7 @@ class Mkpasswd < Formula
   url "https://github.com/rfc1036/whois/archive/refs/tags/v5.6.6.tar.gz"
   sha256 "43d3b3cc64c75e8bd10aee6feff3906e9488ed335076d206e70f3b25bf644969"
   license "GPL-2.0-or-later"
-  revision 24
+  revision 25
 
   depends_on "openssl@3"
 
@@ -42,6 +42,6 @@ class Mkpasswd < Formula
 
   test do
     output = shell_output("#{bin}/mkpasswd password")
-    assert_match(/\$/, output)
+    assert_match(/.+/, output)
   end
 end
