07bb63b5c723d07ff3a5037a10f7df1106b5ec93
Toni D <tonidy@outlook.com>
2026-03-23 09:34:54 +0700

    fix(gen-secp): correct SHA256 checksum

--- Formula/gen-secp.rb
+++ Formula/gen-secp.rb
@@ -2,7 +2,7 @@ class GenSecp < Formula
   desc "Generate secp256k1 keypair CLI"
   homepage "https://github.com/tonidy/gen-secp"
   url "https://files.pythonhosted.org/packages/43/3f/5157304d35c1813ba9b137e81680a3cd71f706954f50243a0d732d2e78a4/gen_secp-0.1.6.tar.gz"
-  sha256 "e44e3e1c106533613ef0503970ed42e8"
+  sha256 "7fc802ab4f721ea9308fba8a1ee3928a0f8c99a8f3f484cca4caf833210a1890"
   license "MIT"
   version "0.1.6"
 
