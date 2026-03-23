6fd8b22f76e5ac4767b279c91a507729c6c9f703
Toni D <tonidy@outlook.com>
2026-03-23 10:29:26 +0700

    fix(mkpasswd): add libxcrypt dependency for macOS

--- Formula/mkpasswd.rb
+++ Formula/mkpasswd.rb
@@ -4,9 +4,10 @@ class Mkpasswd < Formula
   url "https://github.com/rfc1036/whois/archive/refs/tags/v5.6.6.tar.gz"
   sha256 "43d3b3cc64c75e8bd10aee6feff3906e9488ed335076d206e70f3b25bf644969"
   license "GPL-2.0-or-later"
-  revision 26
+  revision 27
 
   depends_on "openssl@3"
+  depends_on "libxcrypt", if: OS.mac?
 
   def install
     # Add missing headers at the very beginning of mkpasswd.c
@@ -25,11 +26,12 @@ class Mkpasswd < Formula
       else
     EOS
 
-    ENV.append "LDFLAGS", "-L/usr/lib -liconv" if OS.mac?
+    if OS.mac?
+      ENV.append "LDFLAGS", "-L#{Formula["libxcrypt"].opt_lib} -liconv"
+    end
 
     have_iconv = OS.mac? ? "HAVE_ICONV=1" : "HAVE_ICONV=0"
 
-    # Linux: use LIBS instead of LDFLAGS so -lcrypt comes after object files
     if OS.linux?
       ENV.append "LIBS", "-lcrypt"
       ENV["PKG_CONFIG_PATH"] = Formula["openssl@3"].opt_lib/"pkgconfig"
