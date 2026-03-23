1a003d9591deac97ed2a129c6d3016d1a99ee270
Toni D <tonidy@outlook.com>
2026-03-23 10:02:12 +0700

    fix(mkpasswd): add -lcrypt for Linux

--- Formula/mkpasswd.rb
+++ Formula/mkpasswd.rb
@@ -4,7 +4,7 @@ class Mkpasswd < Formula
   url "https://github.com/rfc1036/whois/archive/refs/tags/v5.6.6.tar.gz"
   sha256 "43d3b3cc64c75e8bd10aee6feff3906e9488ed335076d206e70f3b25bf644969"
   license "GPL-2.0-or-later"
-  revision 17
+  revision 18
 
   depends_on "openssl@3"
 
@@ -25,7 +25,11 @@ class Mkpasswd < Formula
       else
     EOS
 
-    ENV.append "LDFLAGS", "-L/usr/lib -liconv" if OS.mac?
+    if OS.mac?
+      ENV.append "LDFLAGS", "-L/usr/lib -liconv"
+    else
+      ENV.append "LDFLAGS", "-lcrypt"
+    end
 
     have_iconv = OS.mac? ? "HAVE_ICONV=1" : "HAVE_ICONV=0"
 
